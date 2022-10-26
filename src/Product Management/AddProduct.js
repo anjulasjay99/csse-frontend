ab8a2c3c31f720a025fd4e9ee810bfdcb378256a
SKShehan <shehansilva2013@gmail.com>
2022-10-26 22:31:32 +0530

    React bootstrap added

---
+++ src/Product Management/AddProduct.js
@@ -0,0 +1,12 @@
+import Sidebar from "../Components/Sidebar";
+import styles from '../CSS/addProduct.module.css';
+
+function AddProduct(){
+    return(
+        <div>
+            <div className = {}>
+
+            </div>
+        </div>
+    )
+}
