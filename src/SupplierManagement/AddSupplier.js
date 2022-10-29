0dcc168cb85a1d820773116447429515b5256e82
Rishitha Dilshan <87588442+IT19967080@users.noreply.github.com>
2022-10-29 22:19:24 +0530

    Implement facade patten to add product

--- src/SupplierManagement/AddSupplier.js
+++ src/SupplierManagement/AddSupplier.js
@@ -5,37 +5,51 @@ import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCol, MDBContainer, MDBIc
 import styles from '../CSS/supplier.module.css'
 import { useState } from "react";
 import axios from "axios";
+import {postData} from './Facade';
 export default function AddSupplier() {
 
-    const [ productName , setName ] = useState("");
-    const [ supplierName , setSupplier ] = useState("");
-    const [ priceRate , setPrice ] = useState("");
-    const [ quantityType , setQuantityType ] = useState("");
-    const [ quantity , setQuantity ] = useState("");
-    const [ description , setDescription ] = useState("");
-    const [ image , setImage ] = useState("");
-
-    const onChangeFile = (e) =>{
-        setImage(e.target.files[0]);
-    };
-    function submit() {
-
-        const formData = new FormData();
-
-        formData.append("productName" , productName );
-        formData.append("supplierName" , supplierName );
-        formData.append("productPrice" , priceRate );
-        formData.append("quantityType" , quantityType );
-        formData.append("quantity" , quantity );
-        formData.append("productDescription" , description );
-        formData.append("productImage" , image );
-        console.log(priceRate , );
-        axios.post('http://localhost:8070/products/addProduct' , formData).then((res)=>{
-            console.log(res);
+    const [ businessName , setbusinessName ] = useState("");
+    const [ supplierId , setsupplierId ] = useState("");
+    const [ fullName , setfullName ] = useState("");
+    const [ telephone , settelephone ] = useState("");
+    const [ email , setemail ] = useState("");
+    const [ address , setaddress ] = useState("");
+    const [ state , setstate ] = useState("");
+    const [ zip , setzip ] = useState("");
+   
+    // function postData(url, params = {}) {
+    //     return axios({
+    //       url: url,
+    //       method: "POST",
+    //       params: params
+    //     }).then(res => res.data)
+    //   }
+   
+    function submit(e) {
+        e.preventDefault();
+        console.log("aaa")
+        console.log(businessName);
+       
+        const newSupplier ={
+            businessName,
+            supplierId,
+            fullName,
+            telephone,
+            email,
+            address,
+            state,
+            zip
+        };
+        const url = 'http://localhost:8070/supplier'
+        postData(url, newSupplier).then((res)=>{
+            console.log(res.data);
             alert("Added Succesfully");
         }).catch((err) =>{
             console.log(err);
         })
+        // console.log(newSupplier);
+        // const url = 'http://localhost:8070/supplier'
+        // postData(newSupplier,url)
     }
 
     function clear(){
@@ -47,90 +61,85 @@ export default function AddSupplier() {
     <div className={styles.parent}>
       <Sidebar/>
     <div className={styles.child}>
-    
+
                 <div className={styles.formContainer}>
-                <Form className = {styles.formWrap} encType="multipart/form-data">
-                <h2 style={{color : "#ffff00"}}>Add Product</h2>
+                <Form onSubmit={submit} className = {styles.formWrap} encType="multipart/form-data">
+                <h2  style={{color : "#ffff00",paddingTop:"0px"}}>Add Supplier</h2>
                     <Row>
-                        <Col md={6}>
+                        <Col md={12}>
                             <FormGroup>
-                                <Label for="pid">Product Name</Label>
+                                <Label for="pid">Business Name *</Label>
                                 <Input
                                 className={styles.input}
                                 id="pid"
                                 name="pid"
                                 placeholder="Product Name"
                                 type="text"
-                                value={productName}
-                                onChange={(e) => setName(e.target.value)}
+                                value={businessName}
+                                onChange={(e) => setbusinessName(e.target.value)}
                                 required
                                 />
                             </FormGroup>
                         </Col>
-                        <Col md={6}>
-                        <FormGroup>
-                                <Label for="supName">Supplier Name</Label>
-                                <Input
-                                className={styles.input}
-                                id="supName"
-                                name="supName"
-                                placeholder="Supplier Name"
-                                type="text"
-                                value={supplierName}
-                                onChange={(e) => setSupplier(e.target.value)}
-                                required
-                                />
-                            </FormGroup>
-                        </Col>
+                        
                     </Row>
                     <Row>
                             <FormGroup>
-                                <Label for="pRate">Price Rate</Label>
+                                <Label for="pRate">Full Name</Label>
                                 <Input
                                 className={styles.input}
                                 id="pRate"
                                 name="pRate"
                                 placeholder="Price Rate"
                                 type="text"
-                                value={priceRate}
-                                onChange={(e) => setPrice(e.target.value)}
+                                value={fullName}
+                                onChange={(e) => setfullName(e.target.value)}
                                 required
                                 />
                             </FormGroup>
                     </Row>
+                   
+                    <Row>
+                    <FormGroup>
+                                <Label for="description">Address</Label>
+                                <Input
+                                className={styles.input}
+                                onChange={(e) => setaddress(e.target.value)}
+                                value={address}
+                                id="description"
+                                name="description"
+                                type="text"
+                                required
+                                />
+                    </FormGroup>     
+                    </Row>
                     <Row>
                         <Col md={6}>
-
                         <FormGroup>
-                                <Label for="exampleSelect">
-                                Select Quantity Type
-                                </Label>
-                                <Input
-                                id="exampleSelect"
-                                name="select"
-                                type="select"
-                                onChange={(e) => setQuantityType(e.target.value)}
-                                >
-                                <option>
-                                    Bags
-                                </option>
-                                <option>
-                                    Qb
-                                </option>
-                                </Input>
-                            </FormGroup>
-                        </Col>
-                        <Col md={6}>
-                        <FormGroup>
-                                <Label for="quantity">Quantity</Label>
+                                <Label for="quantity">State</Label>
                                 <Input
                                 className={styles.input}
                                 id="quantity"
                                 name="quantity"
                                 placeholder="Quantity"
                                 type="text"
-                                value={quantity}
-                                onChange={(e) => setQuantity(e.target.value)}
+                                value={state}
+                                onChange={(e) => setstate(e.target.value)}
+                                required
+                                />
+                        </FormGroup>                           
+                        </Col>
+                        <Col md={6}>
+                        <FormGroup>
+                                <Label for="quantity">Zip Code</Label>
+                                <Input
+                                className={styles.input}
+                                id="quantity"
+                                name="quantity"
+                                placeholder="Quantity"
+                                type="text"
+                                value={zip}
+                                onChange={(e) => setzip(e.target.value)}
                                 required
                                 />
                         </FormGroup>                           
@@ -138,49 +147,70 @@ export default function AddSupplier() {
                     </Row>
                     <Row>
                     <FormGroup>
-                                <Label for="description">Description</Label>
-                                <Input
-                                className={styles.input}
-                                onChange={(e) => setDescription(e.target.value)}
-                                id="description"
-                                name="description"
-                                type="textarea"
-                                required
-                                />
-                    </FormGroup>     
-                    </Row>
-
-                    <Row>
-                    <FormGroup>
-                                <Label for="productImage">Product Image</Label>
+                                <Label for="productImage">Email</Label>
                                 <Input
                                 className={styles.input}
                                 id="productImage"
                                 name="productImage"
-                                type="file"
-                                filename="productImage"
-                                onChange={onChangeFile}
+                                type="text"
+                                value={email}
+                                onChange={(e) => setemail(e.target.value)}
+        
                                 required
                                 />
                     </FormGroup>  
                     </Row>
-                    <ButtonGroup>
+                    <Row>
+                        <Col md={6}>
+                        <FormGroup>
+                                <Label for="quantity">Supplier Id</Label>
+                                <Input
+                                className={styles.input}
+                                id="quantity"
+                                name="quantity"
+                                placeholder="Quantity"
+                                type="text"
+                                value={supplierId}
+                                onChange={(e) => setsupplierId(e.target.value)}
+                                required
+                                />
+                        </FormGroup>                           
+                        </Col>
+                        <Col md={6}>
+                        <FormGroup>
+                                <Label for="quantity">Telephone</Label>
+                                <Input
+                                className={styles.input}
+                                id="quantity"
+                                name="quantity"
+                                placeholder="Quantity"
+                                type="text"
+                                value={telephone}
+                                onChange={(e) => settelephone(e.target.value)}
+                                required
+                                />
+                        </FormGroup>                           
+                        </Col>
+                    </Row>
+                    
+                    <ButtonGroup className={styles.btn}>
                         <Button color="danger" style={{float:"left"}} onClick={(e)=>{
                             clear(e);
                         }}>
                             Clear
                         </Button>
-                        <Button color="warning" style={{float:"right"}} onClick={(e)=>{
-                            submit(e);
-                        }}> 
+                        <Button color="warning" style={{float:"right"}} > 
                             Submit
                         </Button>
+                       
                     </ButtonGroup>
 
                 </Form>
+               
+            
             </div>
   
-            
+        
             
 
 
